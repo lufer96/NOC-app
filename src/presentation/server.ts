749fec5a5e6004e3d32bfba1c67ed8d7dae33017
lufer96 <luis-fernando-villa@hotmail.com>
2023-12-02 16:26:37 -0500

    👌 IMPROVE: The cron service is added

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,16 @@
+import { CronService } from './cron/cron-service'
+
 export class ServerApp {
   public static start() {
     console.log('Server starter...')
+
+    CronService.createJob(
+      '*/5 * * * * *',
+      () => {
+        const date = new Date()
+        console.log('5 Seconds', date);
+        
+      }
+    )
   }
 }
