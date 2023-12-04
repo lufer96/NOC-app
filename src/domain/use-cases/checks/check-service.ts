49939ecbaf7dc36fc895457f468dcec5f7990291
lufer96 <luis-fernando-villa@hotmail.com>
2023-12-04 14:56:23 -0500

    👌 IMPROVE: Implement the log repository with file system datasource

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,6 @@
+import { LogRepository } from "../../../repository/log.repository"
+import { LogEntity, LogSeverityLevel } from "../../entities/log.entity"
+
 interface CheckServiceUseCase {
   execute(url: string): Promise<boolean>
 }
@@ -7,6 +10,7 @@ type ErrorCallback = (error: string) => void
 
 export class CheckService implements CheckServiceUseCase {
   constructor(
+    private readonly logRepository: LogRepository,
     private readonly successCallback: SuccessCallback,
     private readonly errorCallBack: ErrorCallback
   ) {}
@@ -16,11 +20,17 @@ export class CheckService implements CheckServiceUseCase {
       const request = await fetch(url)
       if (!request.ok) throw new Error(`Error on check service ${url}`)
 
+      const log = new LogEntity(`Service ${url} working`, LogSeverityLevel.low)
+      this.logRepository.saveLog(log)
       this.successCallback()
 
       return true
     } catch (error) {
-      this.errorCallBack(`${error}`)
+      const errorMessage = `${url} is not ok. ${error}`
+      const log = new LogEntity(`${errorMessage}`, LogSeverityLevel.high)
+      this.logRepository.saveLog(log)
+      this.errorCallBack(errorMessage)
+
       return false
     }
   }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,20 +1,27 @@
-import { CheckService } from '../domain/use-cases/checks/check-service'
-import { CronService } from './cron/cron-service'
+import { CheckService } from "../domain/use-cases/checks/check-service"
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource"
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl"
+import { CronService } from "./cron/cron-service"
 
+const fileSystemLogRepository = new LogRepositoryImpl(
+    new FileSystemDatasource()
+)
 export class ServerApp {
-  public static start() {
-    console.log('Server starter...')
+    public static start() {
+        console.log("Server starter...")
 
-    CronService.createJob('*/5 * * * * *', () => {
-      const url = 'https://google.com'
-      new CheckService(
-        () => console.log(`${url} is ok`),
-        (error) => console.log(error)
-      ).execute(url)
-      // new CheckService(
-      //   () => console.log('success'),
-      //   (error) => console.log(error)
-      // ).execute('http://localhost:3000/posts')
-    })
-  }
+        CronService.createJob("*/5 * * * * *", () => {
+            const url = "https://google.com"
+            new CheckService(
+                fileSystemLogRepository,
+                () => console.log(`${url} is ok`),
+                (error) => console.log(error)
+            ).execute(url)
+            // new CheckService(
+            //  fileSystemLogRepository,
+            //   () => console.log('success'),
+            //   (error) => console.log(error)
+            // ).execute('http://localhost:3000/posts')
+        })
+    }
 }
