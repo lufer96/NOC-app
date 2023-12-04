91937251ab0d77ac5d2107f3fb719e59be97ffdd
lufer96 <luis-fernando-villa@hotmail.com>
2023-12-04 14:36:45 -0500

    👌 IMPROVE: Log repository implementations

---
+++ src/infrastructure/repositories/log.repository.impl.ts
@@ -0,0 +1,17 @@
+import { LogDatasource } from "../../domain/datasources/log.datasource"
+import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity"
+import { LogRepository } from "../../repository/log.repository"
+
+export class LogRepositoryImpl implements LogRepository {
+    // private logDatasource: LogDatasource
+
+    constructor(private readonly logDatasource: LogDatasource) {}
+
+    async saveLog(log: LogEntity): Promise<void> {
+        return this.logDatasource.saveLog(log)
+    }
+
+    async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+        return this.logDatasource.getLogs(severityLevel)
+    }
+}
