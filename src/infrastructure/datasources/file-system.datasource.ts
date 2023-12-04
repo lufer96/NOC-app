abd28c19b40ec6efd9a4327b741ae6d2a1bbb628
lufer96 <luis-fernando-villa@hotmail.com>
2023-12-04 12:14:52 -0500

    👌 IMPROVE: Add functionality to read logs according to their level

--- src/infrastructure/datasources/file-system.datasource.ts
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -1,4 +1,10 @@
-import { appendFileSync, existsSync, mkdirSync, writeFileSync } from "node:fs"
+import {
+    appendFileSync,
+    existsSync,
+    mkdirSync,
+    readFileSync,
+    writeFileSync
+} from "node:fs"
 
 import { LogDatasource } from "../../domain/datasources/log.datasource"
 import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity"
@@ -39,7 +45,32 @@ export class FileSystemDatasource implements LogDatasource {
         if (newLog.level === LogSeverityLevel.high)
             return appendFileSync(this.highLogsPath, logAsJson)
     }
-    getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
-        throw new Error("Method not implemented.")
+
+    private getLogsFromFile = async (
+        logsPath: string
+    ): Promise<LogEntity[]> => {
+        const fileContent = readFileSync(logsPath, {
+            encoding: "utf-8"
+        })
+
+        const logs = fileContent
+            .split("\n")
+            .map((log) => LogEntity.fromJson(log))
+
+        return logs
+    }
+
+    async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+        let logsPath
+
+        if (severityLevel === LogSeverityLevel.low) logsPath = this.allLogsPath
+        if (severityLevel === LogSeverityLevel.medium)
+            logsPath = this.mediumLogsPath
+        if (severityLevel === LogSeverityLevel.high)
+            logsPath = this.highLogsPath
+
+        if (!logsPath) throw new Error(`${severityLevel} not implemented`)
+
+        return await this.getLogsFromFile(logsPath)
     }
 }
