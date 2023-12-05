c19a79f6682111645bb38b0887d19200518657cc
lufer96 <luis-fernando-villa@hotmail.com>
2023-12-05 11:25:58 -0500

    👌 IMPROVE: Added service to send mails using nodemailer

---
+++ src/presentation/email/email.service.ts
@@ -0,0 +1,35 @@
+import nodemailer from "nodemailer"
+import { envs } from "../../config/plugins/envs.plugin"
+
+interface SendEmailOptions {
+    to: string
+    subject: string
+    htmlBody: string
+}
+
+export class EmailService {
+    private transporter = nodemailer.createTransport({
+        service: envs.MAILER_SERVICE,
+        auth: {
+            user: envs.MAILER_EMAIL,
+            pass: envs.MAILER_SECRET_KEY
+        }
+    })
+
+    async sendEmail(options: SendEmailOptions): Promise<boolean> {
+        const { to, subject, htmlBody } = options
+        try {
+            const sentInformation = await this.transporter.sendMail({
+                to,
+                subject,
+                html: htmlBody
+            })
+
+            console.log({ sentInformation })
+
+            return true
+        } catch (error) {
+            return false
+        }
+    }
+}
