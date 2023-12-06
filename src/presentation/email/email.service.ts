3f464d5d0fcb4078ad2f9e80c9e4d82abaad7149
lufer96 <luis-fernando-villa@hotmail.com>
2023-12-06 15:47:40 -0500

    👌 IMPROVE: Send mail with attachments

--- src/presentation/email/email.service.ts
+++ src/presentation/email/email.service.ts
@@ -2,9 +2,15 @@ import nodemailer from "nodemailer"
 import { envs } from "../../config/plugins/envs.plugin"
 
 interface SendEmailOptions {
-    to: string
+    to: string | string[]
     subject: string
     htmlBody: string
+    attachments?: Attachment[]
+}
+
+interface Attachment {
+    filename: string
+    path: string
 }
 
 export class EmailService {
@@ -17,12 +23,13 @@ export class EmailService {
     })
 
     async sendEmail(options: SendEmailOptions): Promise<boolean> {
-        const { to, subject, htmlBody } = options
+        const { to, subject, htmlBody, attachments = [] } = options
         try {
             const sentInformation = await this.transporter.sendMail({
                 to,
                 subject,
-                html: htmlBody
+                html: htmlBody,
+                attachments
             })
 
             console.log({ sentInformation })
@@ -31,5 +38,30 @@ export class EmailService {
         } catch (error) {
             return false
         }
+    }
+
+    async sendEmailWithFileSystemLogs(to: string | string[]) {
+        const subject = "Logs for server"
+        const htmlBody = `
+                <h2>Logs de sistema - NOC</h2>
+                <P>Luis villamizar</P>
+                <p>Ver logs adjuntos</p>
+            `
+        const attachments: Attachment[] = [
+            {
+                filename: "logs-all.log",
+                path: "./logs/logs-all.log"
+            },
+            {
+                filename: "logs-high.log",
+                path: "./logs/logs-high.log"
+            },
+            {
+                filename: "logs-medium.log",
+                path: "./logs/logs-medium.log"
+            }
+        ]
+
+        return this.sendEmail({ to, subject, attachments, htmlBody })
     }
 }
